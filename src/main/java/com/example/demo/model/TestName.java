ae8d005d95a427a7260d84380a0616558fa879d3
tirgani <tirganibadreddine96.gmail.com>
2022-05-31 17:38:31 +0100

    typommmmm8

--- src/main/java/com/example/demo/model/TestName.java
+++ src/main/java/com/example/demo/model/TestName.java
@@ -8,9 +8,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@Component
-public class TestName implements CommandLineRunner {
-    @Autowired
+//@Component
+public class TestName/* implements CommandLineRunner*/ {
+   /* @Autowired
     RoleRepository roleRepository;
     @Autowired
     UserRepository userRepository;
@@ -18,7 +18,7 @@ public class TestName implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         userRepository.deleteAll();
-      
+
       Role admin = new Role("Administrateur");
       Role agre = new Role(2,"Agreculteur");
 
@@ -31,5 +31,5 @@ public class TestName implements CommandLineRunner {
       userRepository.save(tirgani);
 
 
-    }
+    }*/
 }
