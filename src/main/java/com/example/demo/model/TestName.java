04ce7b5b31e267bc85d802a25bd1dd801f3dde7e
tirgani <tirganibadreddine96.gmail.com>
2022-05-31 17:25:17 +0100

    typommmmm5

--- src/main/java/com/example/demo/model/TestName.java
+++ src/main/java/com/example/demo/model/TestName.java
@@ -7,6 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
 @Component
 public class TestName implements CommandLineRunner {
     @Autowired
@@ -17,12 +18,17 @@ public class TestName implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         userRepository.deleteAll();
-        //roleRepository.deleteAll();
-      Role admin = new Role("Administrateur");
-      Role agre = new Role("Agreculteur");
-      roleRepository.save(admin);
-      roleRepository.save(agre);
-      userRepository.save(new User("tirgani","123","tirgani@gmail.com",admin));
+      roleRepository.deleteAll();
+      Role admin = new Role(1,"Administrateur");
+      Role agre = new Role(2,"Agreculteur");
+
+      User tirgani = new User();
+      tirgani.setUserId(1);
+      tirgani.setUsername("tirgani");
+      tirgani.setPassword("123");
+      tirgani.setEmail("tirgani.badreddine@gmail.com");
+      tirgani.setRole(admin);
+      userRepository.save(tirgani);
 
 
     }
