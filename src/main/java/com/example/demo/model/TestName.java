930374c5b2d646ac47cae269af850b644828a584
tirgani <tirganibadreddine96.gmail.com>
2022-05-31 17:44:24 +0100

    typommmmm9

--- src/main/java/com/example/demo/model/TestName.java
+++ src/main/java/com/example/demo/model/TestName.java
@@ -8,18 +8,18 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-//@Component
-public class TestName/* implements CommandLineRunner*/ {
-   /* @Autowired
+@Component
+public class TestName implements CommandLineRunner {
+    @Autowired
     RoleRepository roleRepository;
     @Autowired
     UserRepository userRepository;
 
     @Override
     public void run(String... args) throws Exception {
-        userRepository.deleteAll();
+      //  userRepository.deleteAll();
 
-      Role admin = new Role("Administrateur");
+     /* Role admin = new Role("Administrateur");
       Role agre = new Role(2,"Agreculteur");
 
       User tirgani = new User();
@@ -28,8 +28,10 @@ public class TestName/* implements CommandLineRunner*/ {
       tirgani.setPassword("123");
       tirgani.setEmail("tirgani.badreddine@gmail.com");
       tirgani.setRole(admin);
-      userRepository.save(tirgani);
+      userRepository.save(tirgani);*/
+        Role agre = new Role("Agreculteur");
+        roleRepository.save(agre);
 
 
-    }*/
+    }
 }
