935ef0f1fc4c46263a3ee25cb0a10de0463ffcc0
tirgani <tirganibadreddine96.gmail.com>
2022-05-31 16:56:59 +0100

    typommmmm1

--- src/main/java/com/example/demo/model/TestName.java
+++ src/main/java/com/example/demo/model/TestName.java
@@ -9,20 +9,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 @Component
 public class TestName implements CommandLineRunner {
-    @Autowired
-    RoleRepository roleRepository;
-    @Autowired
-    UserRepository userRepository;
+ //   @Autowired
+ //   RoleRepository roleRepository;
+  //  @Autowired
+  //  UserRepository userRepository;
 
     @Override
     public void run(String... args) throws Exception {
-        userRepository.deleteAll();
-      roleRepository.deleteAll();
-      Role admin = new Role("Administrateur");
-      Role agre = new Role("Agreculteur");
-      roleRepository.save(admin);
-      roleRepository.save(agre);
-      userRepository.save(new User("tirgani","123","tirgani@gmail.com",admin));
+     //   userRepository.deleteAll();
+      //roleRepository.deleteAll();
+   //   Role admin = new Role("Administrateur");
+   //   Role agre = new Role("Agreculteur");
+     // roleRepository.save(admin);
+     // roleRepository.save(agre);
+     // userRepository.save(new User("tirgani","123","tirgani@gmail.com",admin));
 
 
     }
